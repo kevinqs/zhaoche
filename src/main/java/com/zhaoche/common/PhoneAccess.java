01bf0638558634138924e455284e981c67ac614e
Kevin Qiu <kevinqs@gmail.com>
2014-02-05 00:18:26 -0800

    save db data

--- src/main/java/com/zhaoche/common/PhoneAccess.java
+++ src/main/java/com/zhaoche/common/PhoneAccess.java
@@ -2,6 +2,15 @@ package com.zhaoche.common;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "phone_access")
 public class PhoneAccess implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -10,7 +19,9 @@ public class PhoneAccess implements java.io.Serializable {
 	private Date accessTime;
 	private String action;
 	
-	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)	
+	@Column(name = "PHONE_ACCESS_ID", unique = true, nullable = false)
 	public Integer getId() {
 		return id;
 	}
@@ -19,6 +30,7 @@ public class PhoneAccess implements java.io.Serializable {
 		this.id = id;
 	}
 	
+	@Column(name = "PHONE_ID", unique = false, nullable = false, length = 45)	
 	public String getPhoneId() {
 		return phoneId;
 	}
@@ -27,6 +39,7 @@ public class PhoneAccess implements java.io.Serializable {
 		this.phoneId = phoneId;
 	}
 
+	@Column(name = "ACCESS_TIME", nullable = false)	
 	public Date getAccessTime() {
 		return accessTime;
 	}
@@ -35,6 +48,7 @@ public class PhoneAccess implements java.io.Serializable {
 		this.accessTime = accessTime;
 	}
 
+	@Column(name = "ACTION", unique = false, nullable = false, length = 45)	
 	public String getAction() {
 		return action;
 	}
